4b5c290ad6855e05a1ae9562bd7dcca016fd9c6d
Arvinder Dhanoa <ArvinderDhan@gmail.com>
2023-12-10 19:32:18 -0600

    feat: update PoC to auto-update the required ACLs.

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,18 @@ use std::ffi::{c_void, CString};
 use windows::{
     core::{s, w, PCSTR, PCWSTR, PSTR, PWSTR},
     Win32::{
-        Foundation::{HANDLE, PSID},
+        Foundation::{LocalFree, GENERIC_READ, HANDLE, PSID},
         Security::{
-            AllocateAndInitializeSid, Authorization::ConvertSidToStringSidW, FreeSid,
-            Isolation::DeleteAppContainerProfile, SECURITY_APP_PACKAGE_AUTHORITY,
-            SID_AND_ATTRIBUTES,
+            AllocateAndInitializeSid,
+            Authorization::{
+                ConvertSidToStringSidW, GetNamedSecurityInfoA, SetEntriesInAclA,
+                SetNamedSecurityInfoA, EXPLICIT_ACCESS_A, GRANT_ACCESS, SE_FILE_OBJECT,
+                TRUSTEE_IS_OBJECTS_AND_SID, TRUSTEE_IS_SID,
+            },
+            FreeSid,
+            Isolation::DeleteAppContainerProfile,
+            DACL_SECURITY_INFORMATION, OBJECT_INHERIT_ACE, PSECURITY_DESCRIPTOR,
+            SECURITY_APP_PACKAGE_AUTHORITY, SECURITY_DESCRIPTOR, SID_AND_ATTRIBUTES,
         },
         System::{
             SystemServices::{
@@ -124,7 +131,46 @@ fn run_containerized() {
     let cli =
         CString::new(r#""C:\Program Files\VideoLAN\VLC\vlc.exe" "C:\Users\Awsom\Videos\encoded\goin WILD on bookmaker.mp4""#)
             .unwrap();
+
     unsafe {
+        let mut old_acls = std::ptr::null_mut();
+        let mut descriptor = PSECURITY_DESCRIPTOR::default();
+        GetNamedSecurityInfoA(
+            s!(r#"C:\Users\Awsom\Videos"#),
+            SE_FILE_OBJECT,
+            DACL_SECURITY_INFORMATION,
+            None,
+            None,
+            Some(&mut old_acls),
+            None,
+            &mut descriptor,
+        )
+        .unwrap();
+        let acl_entries = EXPLICIT_ACCESS_A {
+            grfAccessPermissions: GENERIC_READ.0,
+            grfAccessMode: GRANT_ACCESS,
+            grfInheritance: OBJECT_INHERIT_ACE,
+            Trustee: windows::Win32::Security::Authorization::TRUSTEE_A {
+                pMultipleTrustee: std::ptr::null_mut(),
+                MultipleTrusteeOperation:
+                    windows::Win32::Security::Authorization::MULTIPLE_TRUSTEE_OPERATION::default(),
+                TrusteeForm: TRUSTEE_IS_SID,
+                TrusteeType: windows::Win32::Security::Authorization::TRUSTEE_TYPE::default(),
+                ptstrName: PSTR(sid.0 as _),
+            },
+        };
+        let mut new_acls = std::ptr::null_mut();
+        SetEntriesInAclA(Some(&[acl_entries]), Some(old_acls), &mut new_acls).unwrap();
+        SetNamedSecurityInfoA(
+            s!(r#"C:\Users\Awsom\Videos"#),
+            SE_FILE_OBJECT,
+            DACL_SECURITY_INFORMATION,
+            None,
+            None,
+            Some(&*new_acls),
+            None,
+        )
+        .unwrap();
         CreateProcessAsUserA(
             HANDLE(0),
             PCSTR::null(),
@@ -140,6 +186,16 @@ fn run_containerized() {
         )
         .unwrap();
         WaitForSingleObject(proc.hProcess, INFINITE);
+        SetNamedSecurityInfoA(
+            s!(r#"C:\Users\Awsom\Videos"#),
+            SE_FILE_OBJECT,
+            DACL_SECURITY_INFORMATION,
+            None,
+            None,
+            Some(&*old_acls),
+            None,
+        )
+        .unwrap();
     }
     unsafe { FreeSid(sid) };
     unsafe { DeleteAppContainerProfile(w!("bax")).unwrap() };
